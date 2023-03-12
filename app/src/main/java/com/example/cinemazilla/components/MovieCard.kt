62bee33b86ab30b796668003894f35782b61a8ab
Adam Hałas <AgopnikH@gmail.com>
2023-03-12 11:27:32 +0100

    Added MovieCard and Internet permission
    Added MovieCard that loads image using coil and shows progress indicator while loading and on error
    Added internet permission

--- app/src/main/java/com/example/cinemazilla/components/MovieCard.kt
+++ app/src/main/java/com/example/cinemazilla/components/MovieCard.kt
@@ -1,13 +1,54 @@
 package com.example.cinemazilla.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImagePainter
+import coil.compose.SubcomposeAsyncImage
+import coil.compose.SubcomposeAsyncImageContent
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
 
+
+private val imageModifier = Modifier
+    .fillMaxWidth()
+    .fillMaxHeight(0.7f)
+@Preview
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MovieCard() {
     Card(onClick = { /*TODO*/ }) {
+        Column(modifier = Modifier
+            .width(150.dp)
+            .height(170.dp)) {
+            SubcomposeAsyncImage(
+                model = "https://images.unsplash.com/photo-1670272502307-5539ca02550a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+                contentDescription = ""
+            ) {
+                val state = painter.state
+                if (state is AsyncImagePainter.State.Loading || state is AsyncImagePainter.State.Error) {
+                    CircularProgressIndicator()
+                } else {
+                    SubcomposeAsyncImageContent()
+                }
+            }
+            Text(text = "TITLE TITLE TITLE TITLE TITLE TITLE TITLE TITLE TITLE TITLE ",
+            maxLines = 2,
+            softWrap = true,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+        }
 
     }
-}
