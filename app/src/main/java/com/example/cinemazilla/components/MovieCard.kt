b26702294a48473dd81367b3ea1e8cfb41eb00a8
Adam Hałas <AgopnikH@gmail.com>
2023-03-12 11:52:59 +0100

    Round image bottom corners and fix progress circle

--- app/src/main/java/com/example/cinemazilla/components/MovieCard.kt
+++ app/src/main/java/com/example/cinemazilla/components/MovieCard.kt
@@ -2,6 +2,7 @@ package com.example.cinemazilla.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
@@ -9,7 +10,9 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,15 +35,19 @@ fun MovieCard() {
         Column(modifier = Modifier
             .width(150.dp)
             .height(170.dp)) {
-            SubcomposeAsyncImage(
-                model = "https://images.unsplash.com/photo-1670272502307-5539ca02550a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-                contentDescription = ""
-            ) {
-                val state = painter.state
-                if (state is AsyncImagePainter.State.Loading || state is AsyncImagePainter.State.Error) {
-                    CircularProgressIndicator()
-                } else {
-                    SubcomposeAsyncImageContent()
+            Row(modifier = imageModifier,
+            horizontalArrangement = Arrangement.Center) {
+                SubcomposeAsyncImage(
+                    modifier = Modifier.clip(RoundedCornerShape(bottomStartPercent = 10, bottomEndPercent = 10)),
+                    model = "https://images.unsplash.com/photo-1674574124473-e91fdcabaefc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+                    contentDescription = ""
+                ) {
+                    val state = painter.state
+                    if (state is AsyncImagePainter.State.Loading || state is AsyncImagePainter.State.Error) {
+                        CircularProgressIndicator()
+                    } else {
+                        SubcomposeAsyncImageContent()
+                    }
                 }
             }
             Text(text = "TITLE TITLE TITLE TITLE TITLE TITLE TITLE TITLE TITLE TITLE ",
